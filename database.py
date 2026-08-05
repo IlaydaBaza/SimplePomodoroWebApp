56e226ffd0a3eb09ce4bcb3830599512bf568f1d
IlaydaBaza <ilaydabaza05@gmail.com>
2026-08-05 14:09:09 +0300

    Update database.py

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 import datetime
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///C:/Users/Lenovo/.gemini/antigravity/scratch/pomodoro_app/backend/database.db"
+SQLALCHEMY_DATABASE_URL =  "sqlite:///./database.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
